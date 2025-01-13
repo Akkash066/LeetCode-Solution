412c1c9e950c5c1442e67b4bb0885aa9edab5795
Akash <86909011+Akash0981@users.noreply.github.com>
2025-01-13 21:23:05 +0530

    Update rmDuplicateFromArray.java
    leetcode problem 26 Remove Duplicates from sorted array

--- rmDuplicateFromArray.java
+++ rmDuplicateFromArray.java
@@ -1,1 +1,14 @@
+//Remove Duplicates From sorted array
 
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int lastUniqueIndex = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(nums[lastUniqueIndex] != nums[i]){
+                nums[lastUniqueIndex + 1] = nums[i];
+                lastUniqueIndex++;
+            }
+        }
+        return lastUniqueIndex + 1;
+    }
+}
