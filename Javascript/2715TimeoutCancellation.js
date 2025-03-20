b64489bbc1fc614971776ca11bd7aa73cac77e15
Akkash <86909011+Akkash066@users.noreply.github.com>
2025-03-20 22:03:43 +0530

    Update and rename 2715Timeout Cancellation.js to 2715TimeoutCancellation.js

--- Javascript/2715TimeoutCancellation.js
+++ Javascript/2715TimeoutCancellation.js
@@ -15,3 +15,12 @@ var cancellable = function(fn, args, t) {
         clearTimeout(timeoutId);
     };
 };
+
+// another option
+function cancellable(fn, args, t) {
+    let timeoutId = setTimeout(() => fn.apply(null, args), t);
+
+    return function cancelFn() {
+        clearTimeout(timeoutId);
+    };
+}
