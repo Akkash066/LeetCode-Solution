bd9684cf085001d35c20d44833257c44d735ca23
Akkash <86909011+Akkash066@users.noreply.github.com>
2025-03-22 23:18:54 +0530

    Update 1TwoSum.js

--- Javascript/1TwoSum.js
+++ Javascript/1TwoSum.js
@@ -1,1 +1,22 @@
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+var twoSum = function(nums, target) {
+    const numMap = {};
+
+    
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        const complement = target - num; 
+
+        
+        if (complement in numMap) {
+            return [numMap[complement], i]; 
+        }
+
+        
+        numMap[num] = i;
+    }
