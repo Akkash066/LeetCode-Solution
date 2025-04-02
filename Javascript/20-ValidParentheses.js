2d10b7214f4f811317f0911a2e8cfcc6a7c1e872
Akkash <86909011+Akkash066@users.noreply.github.com>
2025-04-02 22:32:13 +0530

    Update 20-ValidParentheses.js

--- Javascript/20-ValidParentheses.js
+++ Javascript/20-ValidParentheses.js
@@ -5,3 +5,19 @@
 // Open brackets must be closed by the same type of brackets.
 // Open brackets must be closed in the correct order.
 // Every close bracket has a corresponding open bracket of the same type.
+
+
+var isValid = function(s) {
+    let stack = [];
+    let map = { ')': '(', '}': '{', ']': '[' };
+
+    for (let char of s) {
+        if (char in map) { 
+            if (stack.length === 0 || stack.pop() !== map[char]) return false;
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+};
